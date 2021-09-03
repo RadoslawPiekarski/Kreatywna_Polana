0f6a58d421f4a01e152b78b67b0ec015378b1451
Radek <radoslaw.piekarski@gmail.com>
2021-09-03 23:49:18 +0200

    PEP8 changes in views.py

--- events/views.py
+++ events/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
 
     return render(request, "events/index.html")
 
+
 def events(request):
 
     return render(request, "events/all_events.html")
+
+# single event page
+
 
 def event(request, slug):
 
