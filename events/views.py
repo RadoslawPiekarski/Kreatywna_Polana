ec4bfea40ba6a3eb3c7f1452e6f905a5e1ad9f63
Radek <radoslaw.piekarski@gmail.com>
2021-09-03 22:03:43 +0200

    added views schema (index, events, event)

--- events/views.py
+++ events/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, "events/index.html")
+
+def events(request):
+
+    return render((request, "events/all_events.html"))
+
+def event(request, slug):
+
+    return render(request, "events/event.html")
