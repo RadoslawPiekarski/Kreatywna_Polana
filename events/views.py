19d3c4d482589fffff0ef22beb7e890246a296a0
Radek <radoslaw.piekarski@gmail.com>
2021-09-05 14:02:18 +0200

    added list mocking data

--- events/views.py
+++ events/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
-
+from datetime import date
 
 # Create your views here.
+
+# mocking data:
+all_events = [
+    {
+        "slug": "kreatywne-zabawy",
+        "image": "image1.jpg",
+        "author": "Radek",
+        "date": date(2021, 8, 29),
+        "place": "Olszak",
+        "group": "Misie",
+        "title": "Kreatywne Zabawy",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi.",
+        "is_active": True,
+    },
+    {
+        "slug": "tor-przeszkod",
+        "image": "image2.jpg",
+        "author": "Radek",
+        "date": date(2023, 8, 31),
+        "place": "Osiedle Przemysława",
+        "group": "Skrzaty",
+        "title": "Tor przeszkód",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi.",
+        "is_active": True,
+    },
+    {
+        "slug": "kreatywne-zabawy",
+        "image": "image1.jpg",
+        "author": "Radek",
+        "date": date(2021, 8, 29),
+        "place": "Olszak",
+        "group": "Skrzaty",
+        "title": "Kreatywne Zabawy",
+        "excerpt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in.",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec ligula magna. Praesent aliquet rhoncus massa, blandit rhoncus mauris malesuada in. Sed vulputate, orci id varius rutrum, eros nunc mollis nunc, vitae ornare sem orci sed nisi.",
+        "is_active": True,
+    },
+]
+
 
 def index(request):
 
