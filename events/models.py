c3255dc76833f34de986bc18ce90fea951c3910e
Radek <radoslaw.piekarski@gmail.com>
2021-09-16 21:32:33 +0200

    added Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    date = models.DateField
+    time = models.TimeField
+    time_span = models.TimeField
+    place = models.ForeignKey(Place, null=True, on_delete=models.SET_NULL)
+    hosts = models.ManyToManyField(User)
+    type = models.CharField(max_length=50)
+    image = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField
+    discount_coupons = models.ForeignKey(Discount)
+    is_active = models.BooleanField
+
+
+
