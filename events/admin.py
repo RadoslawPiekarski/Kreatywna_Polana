7aa5560455a26b65caa509f345e33291592bc732
Radek <radoslaw.piekarski@gmail.com>
2021-09-19 21:13:10 +0200

    basic registration of the models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Place, User, Kid, Group, DiscountCoupons, Event, Payment
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Kid)
+admin.site.register(Event)
+admin.site.register(DiscountCoupons)
+admin.site.register(Place)
+admin.site.register(Group)
+admin.site.register(Payment)
