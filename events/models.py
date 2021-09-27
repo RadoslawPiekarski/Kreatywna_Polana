17318f65368bb0236b3dc81163f4ea115c072d9c
Radek <radoslaw.piekarski@gmail.com>
2021-09-27 14:12:35 +0200

    refactored models (missed brackets)

--- events/models.py
+++ events/models.py
@@ -12,7 +12,7 @@ class Place(models.Model):
     street_number = models.CharField(max_length=10)
     district = models.CharField(max_length=50, default="Osiedle Przemysława")
     city = models.CharField(max_length=20, default="Poznań")
-    description = models.TextField
+    description = models.TextField()
 
     def __str__(self):
         return f"{self.name} {self.street} {self.street_number}"
@@ -37,7 +37,7 @@ class User(models.Model):
     surname = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
     phone_number = models.CharField(max_length=20)
-    description = models.TextField
+    description = models.TextField()
     role = models.CharField(max_length=1, choices=ROLES)
 
     def full_name(self):
@@ -49,7 +49,7 @@ class User(models.Model):
 
 class Kid(models.Model):
     kid_name = models.CharField(max_length=20)
-    birth_date = models.DateField
+    birth_date = models.DateField()
     parent = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -58,9 +58,9 @@ class Kid(models.Model):
 
 class DiscountCoupons(models.Model):
     coupon_code = models.CharField(max_length=20)
-    discount_high = models.IntegerField
-    start_date = models.DateField
-    end_date = models.DateField
+    discount_high = models.IntegerField(default=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     def __str__(self):
         return f"{self.start_date} / {self.end_date} {self.discount_high}"
@@ -69,9 +69,9 @@ class DiscountCoupons(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique=True)
-    date = models.DateField
-    time = models.TimeField
-    time_span = models.TimeField
+    date = models.DateField()
+    time = models.TimeField()
+    time_span = models.TimeField()
     image = models.CharField(max_length=100, null=True)
     place = models.ForeignKey(Place, null=True, on_delete=models.SET_NULL)
     instructor = models.ManyToManyField(User)
@@ -79,9 +79,9 @@ class Event(models.Model):
     group = models.ForeignKey(Group, null=True, on_delete=models.SET_NULL)
     kids = models.ManyToManyField(Kid)
     excerpt = models.CharField(max_length=200)
-    content = models.TextField
+    content = models.TextField()
     discount_coupons = models.ManyToManyField(DiscountCoupons)
-    is_active = models.BooleanField
+    is_active = models.BooleanField()
 
     def __str__(self):
         return f"{self.title} {self.group} {self.place} {self.date} {self.time}"
@@ -90,8 +90,8 @@ class Event(models.Model):
 class Payment(models.Model):
     kid = models.ForeignKey(Kid, null=True, on_delete=models.SET_NULL)
     event = models.ForeignKey(Event, null=True, on_delete=models.SET_NULL)
-    is_paid = models.BooleanField
-    date = models.DateField
+    is_paid = models.BooleanField()
+    date = models.DateField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     coupon = models.ForeignKey(DiscountCoupons, null=True, on_delete=models.CASCADE)
 
