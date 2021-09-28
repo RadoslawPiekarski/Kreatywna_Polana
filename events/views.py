02f87ed0491a0d64a9e29743ac80da3a921c9ebe
Radek <radoslaw.piekarski@gmail.com>
2021-09-28 14:13:42 +0200

    cleared comments and help data/functions

--- events/views.py
+++ events/views.py
@@ -7,8 +7,6 @@ from .models import Event
 
 def index(request):
     latest_events = Event.objects.all().order_by("-date")[:8]
-    # sorted_events = sorted(all_events, key=get_date)
-    # latest_events = sorted_events[-3:]
     return render(request, "events/index.html", {"events": latest_events})
 
 
@@ -23,7 +21,6 @@ def events(request):
 
 def event_detail(request, slug):
     identified_event = get_object_or_404(Event, slug=slug)
-    # identified_event = next(event for event in all_events if event['slug'] == slug)
     return render(request, "events/event_detail.html", {
         "event": identified_event,
         "instructors": identified_event.instructor.all()
