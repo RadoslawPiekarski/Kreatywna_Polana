4822926119a65f3d56aca3fc9142934e5fdebcd8
Radek <radoslaw.piekarski@gmail.com>
2021-09-28 11:20:22 +0200

    changed display Ewent in admin (plus added filters)

--- events/admin.py
+++ events/admin.py
@@ -4,9 +4,14 @@ from .models import Place, User, Kid, Group, DiscountCoupons, Event, Payment
 # Register your models here.
 
 
+class EventAdmin(admin.ModelAdmin):
+    list_filter = ("place", "group",)
+    list_display = ("title", "date", "place", "group")
+
+
 admin.site.register(User)
 admin.site.register(Kid)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
 admin.site.register(DiscountCoupons)
 admin.site.register(Place)
 admin.site.register(Group)
