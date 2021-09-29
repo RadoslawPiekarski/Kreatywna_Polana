25cee9db780a0578777ef359b2878ce60bc89704
Radek <radoslaw.piekarski@gmail.com>
2021-09-29 08:52:23 +0200

    imported forms file

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Event
 from django.http import HttpResponseRedirect
+from .forms import LoginForm
 
 # Create your views here.
 
