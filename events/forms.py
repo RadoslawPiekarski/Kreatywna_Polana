6c0567b3eace1a6ff6664b3139905e1803a3f9eb
Radek <radoslaw.piekarski@gmail.com>
2021-09-29 14:18:26 +0200

    changed attributes in CreateUserForm

--- events/forms.py
+++ events/forms.py
@@ -3,15 +3,15 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20, label="Login")
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label="Haslo")
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label="Hasło")
 
 
 class CreateUserForm(forms.Form):
-    login = forms.CharField(max_length=20, unique=True, label="Login")
+    login = forms.CharField(max_length=20, label="Login")
     password = forms.CharField(max_length=20, label="Hasło")
     name = forms.CharField(max_length=20, label="Imię")
     surname = forms.CharField(max_length=50, label="Nazwisko")
-    email = forms.EmailField(max_length=50, unique=True, label="E-mail")
+    email = forms.EmailField(max_length=50, label="E-mail")
     phone_number = forms.CharField(max_length=20, label="Numer telefonu")
 
     # kid
