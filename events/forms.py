27ce43b41e7f9306325c380419f6c5a498fdde91
Radek <radoslaw.piekarski@gmail.com>
2021-09-29 13:19:12 +0200

    added simple css form

--- events/forms.py
+++ events/forms.py
@@ -2,5 +2,10 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput)
+    username = forms.CharField(max_length=20, label="Login")
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label="Haslo")
+
+
+
+
+# required=False - pole niewymagane
