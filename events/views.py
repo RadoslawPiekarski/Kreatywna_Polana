5922f9ee1568896bb5c9c2b1dc0744b819279b37
Radek <radoslaw.piekarski@gmail.com>
2021-09-29 13:43:41 +0200

    added create user views

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Event
 from django.http import HttpResponseRedirect
-from .forms import LoginForm
+from .forms import LoginForm, CreateUserForm
 
 # Create your views here.
 
@@ -39,5 +39,18 @@ def login(request):
 
     # if GET create and render form
     return render(request, "events/login_form.html", {
+        "form": form
+    })
+
+
+def create_user(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/events/")
+
+    return render(request, "events/create_user.html", {
         "form": form
     })
