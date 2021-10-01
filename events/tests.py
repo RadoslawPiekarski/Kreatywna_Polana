dea439e6a21acd91522c65eb50dac0c5c68cf883
Radek <radoslaw.piekarski@gmail.com>
2021-10-01 15:51:35 +0200

    initial settings and first testing tests

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import datetime
+from unittest.mock import patch
 
-# Create your tests here.
+import pytest
+import http
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.models import User
+from django.test import Client
+
+
+# HomePageView
+def test_index(client):
+    response = client.get("")
+    assert response.status_code == 200
+
+
+# AllEventsView
+def test_events(client):
+    response = client.get('/events/')
+    assert response.status_code == 200
