65b0ca9a1821705b0d3d9d55c3d7e679b3a9ca4f
Radek <radoslaw.piekarski@gmail.com>
2021-10-08 14:17:10 +0200

    added EventForm

--- events/forms.py
+++ events/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+# from django.forms import ModelForm
+from .models import Event
 
 
 class LoginForm(forms.Form):
@@ -10,7 +12,7 @@ class LoginForm(forms.Form):
 class CreateUserForm(forms.Form):
     username = forms.CharField(max_length=20, label="Login")
     password = forms.CharField(max_length=20, label="Hasło", widget=forms.PasswordInput)
-    password2 = forms.CharField(max_length=20, label ="Hasło2", widget=forms.PasswordInput)
+    password2 = forms.CharField(max_length=20, label="Hasło2", widget=forms.PasswordInput)
     firstname = forms.CharField(max_length=20, label="Imię")
     lastname = forms.CharField(max_length=50, label="Nazwisko")
     email = forms.EmailField(max_length=50, label="E-mail")
@@ -22,3 +24,32 @@ class CreateUserForm(forms.Form):
 
 
 # required=False - pole niewymagane
+
+# TODO dodać komunikaty błędów
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = "__all__"
+        exclude = ["kids"]
+        labels = {
+            "title": "Nazwa wydarzenia",
+            "slug": "Skrót nazwy",
+            "date": "Data wydarzenia (yyyy-mm-dd)",
+            "time": "Godzina rozpoczęcia",
+            "time_span": "Godzina zakończenia",
+            "image": "Adres obrazka",
+            "place": "Miejsce",
+            "instructor": "Prowadzący zajęcia",
+            "type": "Rodzaj zajęć",
+            "group": "Grupa",
+            "excerpt": "Krótki opis",
+            "content": "Opis",
+            "discount_coupons": "Kupony rabatowe",
+            "is_active": "Wydarzenie jest dostępne",
+        }
+        error_messages = {
+            "title": {
+                "required": "Nazwa nie może być pusta!",
+                "max_length": "Maksymalna liczba znaków: 50"
+            }
+        }
