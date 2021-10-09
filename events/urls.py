18667b3331a9746b5797539b6f5e96261f9f7bfc
Radek <radoslaw.piekarski@gmail.com>
2021-10-10 00:28:01 +0200

    changed AddEvent url

--- events/urls.py
+++ events/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.index, name="main-page"),
     path("events/", views.Events.as_view(), name="all-events"),
+path("events/add_event", views.AddEvent.as_view(), name="add-event"),
     path("events/<slug:slug>", views.event_detail, name="single-event"),
     path("login/", views.Login.as_view(), name="login"),
     path("create-user/", views.CreateUser.as_view(), name="create-user"),
     path("parents_info/", views.parents_info_page, name="parents-info"),
-    path("events/add_event", views.Events.as_view(), name="add-event")
+
 ]
