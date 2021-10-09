cb4159273cd365607382e8d0d11a94ad489d05f3
Radek <radoslaw.piekarski@gmail.com>
2021-10-10 01:34:34 +0200

    changed typo

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="main-page"),
     path("events/", views.Events.as_view(), name="all-events"),
-path("events/add_event", views.AddEvent.as_view(), name="add-event"),
+    path("events/add_event", views.AddEvent.as_view(), name="add-event"),
     path("events/<slug:slug>", views.event_detail, name="single-event"),
     path("login/", views.Login.as_view(), name="login"),
     path("create-user/", views.CreateUser.as_view(), name="create-user"),
